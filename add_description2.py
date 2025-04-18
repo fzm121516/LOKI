41d403d5359dae86e1a1b328e5e72cd03d472dac
fan <3223643263@qq.com>
2025-04-18 16:59:58 +0800

    0418

--- add_description2.py
+++ add_description2.py
@@ -1,27 +1,30 @@
 import json
 
-# 读取JSON文件
 with open('true_or_false_with_description.json', 'r', encoding='utf-8') as f:
     data = json.load(f)
 
-# 处理每个条目
 for item in data:
     if 'description' in item and 'question' in item:
-        # 找到<video>\n的位置
         video_tag_pos = item['question'].find('<video>\n')
         if video_tag_pos != -1:
-            # 在<video>\n后面插入description内容
             insert_pos = video_tag_pos + len('<video>\n')
-            new_question = (item['question'][:insert_pos] + 
-                           item['description'] + 
-                           item['question'][insert_pos:])
+            
+            # 确保 description 是字符串，并在后面加 \n
+            description = item['description']
+            if isinstance(description, list):
+                description = " ".join(description)  # 或者 "".join(description)
+            
+            new_question = (
+                item['question'][:insert_pos] + 
+                description + "\n" +  # 这里加 \n
+                item['question'][insert_pos:]
+            )
             item['question'] = new_question
         
-        # 删除description字段
         del item['description']
 
-# 保存修改后的JSON文件
-with open('true_or_false_modified.json', 'w', encoding='utf-8') as f:
+with open('true_or_false_VDP.json', 'w', encoding='utf-8') as f:
     json.dump(data, f, ensure_ascii=False, indent=4)
 
-print("处理完成，结果已保存到true_or_false_modified.json")
+print("处理完成，结果已保存到 true_or_false_modified.json")
+
