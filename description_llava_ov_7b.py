8a6fb378e214ab9d2e18844a2da3fb2dadfc43ee
fan <3223643263@qq.com>
2025-04-25 19:41:10 +0800

    0425

--- description_llava_ov_7b.py
+++ description_llava_ov_7b.py
@@ -14,19 +14,20 @@ np.random.seed(seed)
 random.seed(seed)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
-# pip install git+https://github.com/LLaVA-VL/LLaVA-NeXT.git
+from operator import attrgetter
 from llava.model.builder import load_pretrained_model
 from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
 from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
 from llava.conversation import conv_templates, SeparatorStyle
 
+import torch
+import cv2
+import numpy as np
 from PIL import Image
 import requests
 import copy
-import torch
-
-import sys
 import warnings
+from decord import VideoReader, cpu
 
 warnings.filterwarnings("ignore")
 pretrained = "/root/zgp2/fanzheming/LVLM/llava-onevision-qwen2-7b-ov"
@@ -37,75 +38,53 @@ tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained
 
 model.eval()
 
-# pip install git+https://github.com/LLaVA-VL/LLaVA-NeXT.git
-from llava.model.builder import load_pretrained_model
-from llava.mm_utils import get_model_name_from_path, process_images, tokenizer_image_token
-from llava.constants import IMAGE_TOKEN_INDEX, DEFAULT_IMAGE_TOKEN, DEFAULT_IM_START_TOKEN, DEFAULT_IM_END_TOKEN, IGNORE_INDEX
-from llava.conversation import conv_templates, SeparatorStyle
-from PIL import Image
-import requests
-import copy
-import torch
-import sys
-import warnings
-from decord import VideoReader, cpu
-import numpy as np
-warnings.filterwarnings("ignore")
-
-def load_video(self, video_path, max_frames_num,fps=1,force_sample=False):
-    if max_frames_num == 0:
-        return np.zeros((1, 336, 336, 3))
-    vr = VideoReader(video_path, ctx=cpu(0),num_threads=1)
+def load_video(video_path, max_frames_num):
+    if type(video_path) == str:
+        vr = VideoReader(video_path, ctx=cpu(0))
+    else:
+        vr = VideoReader(video_path[0], ctx=cpu(0))
     total_frame_num = len(vr)
-    video_time = total_frame_num / vr.get_avg_fps()
-    fps = round(vr.get_avg_fps()/fps)
-    frame_idx = [i for i in range(0, len(vr), fps)]
-    frame_time = [i/fps for i in frame_idx]
-    if len(frame_idx) > max_frames_num or force_sample:
-        sample_fps = max_frames_num
-        uniform_sampled_frames = np.linspace(0, total_frame_num - 1, sample_fps, dtype=int)
-        frame_idx = uniform_sampled_frames.tolist()
-        frame_time = [i/vr.get_avg_fps() for i in frame_idx]
-    frame_time = ",".join([f"{i:.2f}s" for i in frame_time])
+    uniform_sampled_frames = np.linspace(0, total_frame_num - 1, max_frames_num, dtype=int)
+    frame_idx = uniform_sampled_frames.tolist()
     spare_frames = vr.get_batch(frame_idx).asnumpy()
-    # import pdb;pdb.set_trace()
-    return spare_frames,frame_time,video_time
-pretrained = "/root/zgp2/fanzheming/LVLM/llava-onevision-qwen2-7b-ov"
-model_name = "llava_qwen"
-device = "cuda"
-device_map = "auto"
-tokenizer, model, image_processor, max_length = load_pretrained_model(pretrained, None, model_name, torch_dtype="bfloat16", device_map=device_map)  # Add any other thing you want to pass in llava_model_args
-model.eval()
-
-max_frames_num = "64"
+    return spare_frames  # (frames, height, width, channels)
 
 
 
 @torch.inference_mode()
 def generate_description(video_path):
     
-    video,frame_time,video_time = load_video(video_path, max_frames_num, 1, force_sample=True)
-    video = image_processor.preprocess(video, return_tensors="pt")["pixel_values"].cuda().bfloat16()
-    video = [video]
-    conv_template = "qwen_2"  # Make sure you use correct chat template for different models
-    time_instruciton = f"The video lasts for {video_time:.2f} seconds, and {len(video[0])} frames are uniformly sampled from it. These frames are located at {frame_time}.Please answer the following questions related to this video."
-    question = DEFAULT_IMAGE_TOKEN + f"{time_instruciton}\nYou are shown a video. Please describe the content of this video in detail."
+    video_frames = load_video(video_path, 16)
+    print(video_frames.shape) # (16, 1024, 576, 3)
+    image_tensors = []
+    frames = image_processor.preprocess(video_frames, return_tensors="pt")["pixel_values"].half().cuda()
+    image_tensors.append(frames)
+
+    # Prepare conversation input
+    conv_template = "qwen_1_5"
+    question = f"{DEFAULT_IMAGE_TOKEN}\nDescribe what's happening in this video."
+
     conv = copy.deepcopy(conv_templates[conv_template])
     conv.append_message(conv.roles[0], question)
     conv.append_message(conv.roles[1], None)
     prompt_question = conv.get_prompt()
+
     input_ids = tokenizer_image_token(prompt_question, tokenizer, IMAGE_TOKEN_INDEX, return_tensors="pt").unsqueeze(0).to(device)
+    image_sizes = [frame.size for frame in video_frames]
+
+    # Generate response
     cont = model.generate(
         input_ids,
-        images=video,
-        modalities= ["video"],
+        images=image_tensors,
+        image_sizes=image_sizes,
         do_sample=False,
         temperature=0,
         max_new_tokens=4096,
+        modalities=["video"],
     )
-    text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)[0].strip()
-    print(text_outputs)
-    return text_outputs
+    text_outputs = tokenizer.batch_decode(cont, skip_special_tokens=True)
+    print(text_outputs[0])
+    return text_outputs[0]
 
 def process_videos(json_path):
     # 读取JSON文件
